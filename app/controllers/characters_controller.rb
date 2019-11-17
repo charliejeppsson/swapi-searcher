de2e90012d01489e75003c23e59e16e89de505f4
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-11-17 22:41:36 +0100

    Set up pagination and refactor index page

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,8 +1,10 @@
 class CharactersController < ApplicationController
   def index
+    page = params[:page] || 1
     search_term = params[:search].to_s.strip
     swapi_request = Swapi::SearchCharacters.new
-    swapi_response_body = swapi_request.send_request(search_term)
+    swapi_response_body = swapi_request.send_request(search_term, page)
+    @next_page_exists = swapi_response_body["next"].present?
     @characters = swapi_response_body["results"]
   end
 end
--- app/services/swapi/search_characters.rb
+++ app/services/swapi/search_characters.rb
@@ -1,15 +1,17 @@
 module Swapi
   class SearchCharacters
-    def send_request(search_term = nil)
+    def send_request(search_term = nil, page = nil)
       connection = Faraday.new(url: "https://swapi.co/api/people/")
       res = connection.get do |req|
         req.params[:search] = search_term if search_term.present?
+        req.params[:page] = page if page.present?
       end
 
       if res.success?
         return JSON.parse(res.body)
       else
-        puts "Something went wrong. Res body: #{JSON.parse(res.body)}"
+        puts "Error: #{res.status}, #{res.reason_phrase}"
+        return { results: [] }
       end
     end
   end
