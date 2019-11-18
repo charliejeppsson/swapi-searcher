92533ad437b8d26660dea4889c510bf77f19ed92
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-11-18 21:01:57 +0100

    Change param name to search_term

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,7 @@
 class CharactersController < ApplicationController
   def index
     page = params[:page] || 1
-    search_term = params[:search].to_s.strip
+    search_term = params[:search_term].to_s.strip
     swapi_request = Swapi::SearchCharacters.new
     swapi_response_body = swapi_request.send_request(search_term, page)
     @next_page_exists = swapi_response_body["next"].present?
